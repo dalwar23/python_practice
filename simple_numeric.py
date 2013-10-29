4a0507320c87bed12c2812fec3a8ff66fe02ec40
Arif <dharif23@gmail.com>
2013-10-29 15:50:09 +0600

    Create simple_numeric.py
    Numeric calculations

---
+++ simple_numeric.py
@@ -0,0 +1,34 @@
+# !/usr/bin/python -tt
+# import system library
+import sys
+# simple numeric calculations
+var1 = 45
+var2 = 46
+var3 = 47
+
+# plus
+var4 = var1 + var2 + var3
+# print the sum of all three variables
+print ("Add:",var4)
+
+#user input
+var5 = input("Please provide interger: ")
+#view user input variable
+print ("You typed:",var5)
+
+# substraction
+var6 = var3 - var1
+print("Sub:",var6)
+
+# multiply
+var7 = var1 * var2
+print("multiply:",var7)
+
+# division
+var8 = var2/var3
+print ("division:",var8)
+
+#critical calculations
+var9 = pow(var1,2)
+var10 = var9*(var2/var3)-var7+(var1*var6)
+print ("complex:",var10)
